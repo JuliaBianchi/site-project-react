010cb1bfd5245c02c6e2ca4b7b78be41fa9e7141
Júlia <juliaspaderbianchi@gmail.com>
2022-12-23 15:29:19 -0300

    Exemplo aula 09.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import './css/style.css'
+import loginImage from './assets/images/login.png'
 
-function App() {
-  
-  return (
-    <div>
-      Olá Mundo!
-    </div>
-  );
+const App = () => {
+
+  let mensagemLogin = "Informe os dados do usuário para acessa a plataforma."
+
+  const getNamePlatform = () => {
+    return "Bianchi Corporation"
+  }
+
+
+  return(
+    <>
+      <div> 
+        <img src={ loginImage } alt="Imagem de Login" style={{ width:200 }} /> 
+      </div>
+
+      <h1>{getNamePlatform()}</h1>
+
+      <span className='informativo'>{mensagemLogin}</span> <br/>
+
+      <input placeholder="Username:"  type="text" /> <br/>
+      <input placeholder="Password:" type="password" /> <br/>
+
+      <button>ENTRAR</button>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Teste from './Teste';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Teste />
+    <App />
   </React.StrictMode>
 );
 
